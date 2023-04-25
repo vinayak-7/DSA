3794c087760ccfcab2d079cd9a6d503d213ba606
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-04-25 15:30:12 +0530

    Added solution - LeetHub

--- Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
@@ -5,33 +5,66 @@ using namespace std;
 // } Driver Code Ends
 class Solution
 {
+    // using priority queue
+    // vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    // {
+    //     // Code here
+    //     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+    //     vector<int> dist(V);
+    //     for(int i = 0 ; i < V ; i++) dist[i] = 1e9;
+        
+    //     dist[S] = 0;
+    //     pq.push({0,S});
+        
+    //     while(!pq.empty())
+    //     {
+    //         int dis = pq.top().first;
+    //         int node = pq.top().second;
+    //         pq.pop();
+            
+    //         for(auto it: adj[node]){
+    //             int edgeWeight = it[1];
+    //             int adjNode = it[0];
+                
+    //             if(dis + edgeWeight < dist[adjNode])
+    //             {
+    //                 dist[adjNode] = dis + edgeWeight;
+    //                 pq.push({dist[adjNode],adjNode});
+    //             }
+    //         }
+    //     }
+        
+    //     return dist;
+    // }
 	public:
 	//Function to find the shortest distance of all the vertices
     //from the source vertex S.
+    // using set
     vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
     {
         // Code here
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        vector<int> dist(V);
-        for(int i = 0 ; i < V ; i++) dist[i] = 1e9;
+        set<pair<int,int>> st;
+        vector<int> dist(V,1e9);
         
+        st.insert({0,S});
         dist[S] = 0;
-        pq.push({0,S});
         
-        while(!pq.empty())
+        while(!st.empty())
         {
-            int dis = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
+            auto it = *(st.begin());
+            int dis = it.first;
+            int node = it.second;
+            st.erase(it);
             
-            for(auto it: adj[node]){
+            for(auto it:adj[node])
+            {
                 int edgeWeight = it[1];
                 int adjNode = it[0];
                 
                 if(dis + edgeWeight < dist[adjNode])
                 {
                     dist[adjNode] = dis + edgeWeight;
-                    pq.push({dist[adjNode],adjNode});
+                    st.insert({dist[adjNode],adjNode});
                 }
             }
         }
