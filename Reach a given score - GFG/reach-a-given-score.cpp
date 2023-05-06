a5f4fcefb01d22c766c7fdd5e49b918be8adc02c
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-05-07 00:24:03 +0530

    Added solution - LeetHub

--- Reach a given score - GFG/reach-a-given-score.cpp
+++ Reach a given score - GFG/reach-a-given-score.cpp
@@ -6,21 +6,23 @@ using namespace std;
 
 // } Driver Code Ends
 // Complete this function
-long long game(long long int n,vector<int> &opt , int i)
+long long game(long long int n,vector<int> &opt , int i,vector<vector<int>> &dp)
 {
     if(n==0) return 1;
     if(i < 0) return n == 0;
     
-    long long move = n - opt[i] >= 0 ? game(n-opt[i],opt,i): 0;
-    long long dontMove = game(n,opt,i-1);
+    if(dp[i][n] != -1) return dp[i][n];
     
-    return move+dontMove;
+    long long move = n - opt[i] >= 0 ? game(n-opt[i],opt,i,dp): 0;
+    long long dontMove = game(n,opt,i-1,dp);
+    
+    return dp[i][n] = move + dontMove;
 }
 long long int count(long long int n)
 {
 	vector<int> opt{3,5,10};
-	
-	return game(n,opt,2);
+	vector<vector<int>> dp(3 , vector<int>(n+1,-1));
+	return game(n,opt,2,dp);
 	
 }
 
