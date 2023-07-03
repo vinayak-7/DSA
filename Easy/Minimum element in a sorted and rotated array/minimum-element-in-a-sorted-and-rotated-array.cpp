df01dfe2d1514e88020c8691c2afb6a43285ac5c
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-07-04 01:05:16 +0530

    Updated Solution - GfG to GitHub

--- Easy/Minimum element in a sorted and rotated array/minimum-element-in-a-sorted-and-rotated-array.cpp
+++ Easy/Minimum element in a sorted and rotated array/minimum-element-in-a-sorted-and-rotated-array.cpp
@@ -6,10 +6,20 @@ using namespace std;
 class Solution{
 public:
     int findMin(int arr[], int n){
-        //complete the function here
-        sort(arr,arr+n);
         
-        return arr[0];
+        int low = 0, high=n-1;
+        while(low < high){
+            // if already sorted
+            if(arr[low] < arr[high]) return arr[low];
+            int mid= low + (high-low)/2;
+            // sample example : {4, 5, 6, 7, 0, 1, 2}
+            if(arr[low] <= arr[mid]){
+                low = mid+1;
+            }else{
+                high = mid;
+            }
+        }
+        return arr[low];
     }
 };
 
