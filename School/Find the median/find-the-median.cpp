c0875cfeff22263cafe569bb75ecaf57970886e7
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-07-04 00:17:19 +0530

    Added Solution - GfG to GitHub

---
+++ School/Find the median/find-the-median.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+	public:
+		int find_median(vector<int> v)
+		{
+		    // Code here.
+		    sort(v.begin(),v.end());
+		    int index = v.size()/2;
+		    
+		    
+		    if(v.size() % 2 != 0) return v[index];
+		    
+		    return (v[index] + v[index-1])/2;
+		    
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	vector<int> v(n);
+    	for(int i = 0; i < n; i++)
+    		cin>>v[i];
+    	Solution ob;
+    	int ans = ob.find_median(v);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
