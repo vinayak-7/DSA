7109d744d59037625f3607df8c6f7cd2237a8080
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-07-07 01:04:38 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Nth Natural Number/nth-natural-number.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+class Solution{
+	public:
+    	long long findNth(long long N)
+    {
+        // code here.
+        long long ans=0,x=1;
+           while(N){
+               ans += N % 9 * x;
+               N /= 9;
+               x *= 10;
+           }
+           return ans ;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		long long n , ans;
+		cin>>n;
+		Solution obj;
+		ans = obj.findNth(n);
+		cout<<ans<<endl;
+	}
+}
+
+
+// } Driver Code Ends
