44d5da1c6a3122763b7477e90751f2e9110a6aad
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-07-31 16:08:43 +0530

    Updated Solution - GfG to GitHub

--- Easy/Roman Number to Integer/roman-number-to-integer.cpp
+++ Easy/Roman Number to Integer/roman-number-to-integer.cpp
@@ -10,23 +10,25 @@ using namespace std;
 
 class Solution {
   public:
-    int check(char a){
-        if(a == 'I') return 1;
-        else if(a == 'V') return 5;
-        else if(a == 'L') return 50;
-        else if(a == 'X') return 10;
-        else if(a == 'C') return 100;
-        else if(a == 'D') return 500;
-        else if(a == 'M') return 1000;
-        else return 0;
-    }
     int romanToDecimal(string &str) {
-        int n = str.length();
+        unordered_map<char, int>m;
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
+
         int ans = 0;
-        for(int i = 0; i<n; i++){
-            if(check(str[i]) >= check(str[i+1])) ans += (check(str[i]));
-            else 
-                ans = ans - (check(str[i]));
+
+        for(int i=0; i<str.length(); i++){
+            if(m[str[i]] < m[str[i+1]]){
+                ans = ans - m[str[i]];
+            }
+            else if(m[str[i]] >= m[str[i+1]]){
+                ans = ans + m[str[i]];
+            }
         }
         return ans;
     }
