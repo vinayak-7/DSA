6e51fd7c4ee9706e2314f6b9a8cba4dd4166c47d
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-08-02 22:07:42 +0530

    Added solution - LeetHub

--- Count BST nodes that lie in a given range - GFG/count-bst-nodes-that-lie-in-a-given-range.cpp
+++ Count BST nodes that lie in a given range - GFG/count-bst-nodes-that-lie-in-a-given-range.cpp
@@ -87,19 +87,29 @@ Node* buildTree(string str)
 //Function to count number of nodes in BST that lie in the given range.
 class Solution{
 public:
-    int cnt=0;
-    int getCount(Node *root, int l, int h)
+    int getCount(Node* root, int low, int high)
     {
-      if(root==NULL) return 0;
-      
-      getCount(root->left,l,h);
-      if(root->data>=l && root->data<=h) cnt++;
-      getCount(root->right,l,h);
-      
-      return cnt;
+    	if (!root) 
+    	    return 0;
+    
+    	//if data at current node is equal to lower and upper range, we return 1.
+    	if (root->data == high && root->data == low)
+    		return 1;
+    
+    	//if data at current node is within range then we include it in count 
+    	//and call function recursively for its left and right children.
+    	if (root->data <= high && root->data >= low)
+    		return 1+getCount(root->left,low,high)+getCount(root->right,low,high);
+    
+    	//else if data at current node is smaller than lower range then
+    	//we call function recursively only for right child.
+    	else if (root->data < low)
+    		return getCount(root->right, low, high);
+    
+    	//else we call function recursively only for left child.
+    	else return getCount(root->left, low, high);
     }
 };
-
 
 //{ Driver Code Starts.
 
