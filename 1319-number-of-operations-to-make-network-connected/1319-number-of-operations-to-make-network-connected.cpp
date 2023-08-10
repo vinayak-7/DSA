4a2d0b1ecd001b781daaecaaa787a0985fbd43de
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-08-10 16:06:45 +0530

    Time: 108 ms (85.64%), Space: 51.4 MB (29.42%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-    void bfs(vector<vector<int>>& adj,int curr, vector<bool>& vis ){
-        queue<int>q;
-        q.push(curr);
-        vis[curr]=true;
-        while(!q.empty()){
-            curr=q.front();
-            q.pop();
-            for(auto& node:adj[curr]){
-                if(!vis[node]){ 
-                    vis[node]=true;
-                    q.push(node);
-                }
+    void dfs(int node, vector<vector<int>>& adj, vector<bool>& visit) {
+        visit[node] = true;
+        for (int neighbor : adj[node]) {
+            if (!visit[neighbor]) {
+                dfs(neighbor, adj, visit);
             }
         }
     }
-    int makeConnected(int n, vector<vector<int>>& connections) {
-        if(connections.size()<n-1) return -1; // if the number of connections is less than n-1 that means that we cannot connect all nodes reachable
 
-        vector<bool>vis(n,false);
-        vector<vector<int>>adj(n); // adjacency matrix
-        for(auto& c:connections){
-            adj[c[0]].push_back(c[1]);
-            adj[c[1]].push_back(c[0]);
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if (connections.size() < n - 1) {
+            return -1;
         }
-        int conn=-1;
-        for(int i=0;i<n;++i){
-            if(!vis[i]){ // if not visited, a connected component is present that is unvisited
-                ++conn; 
-                bfs(adj,i,vis);
+
+        vector<vector<int>> adj(n);
+        for (auto& connection : connections) {
+            adj[connection[0]].push_back(connection[1]);
+            adj[connection[1]].push_back(connection[0]);
+        }
+
+        int numberOfConnectedComponents = 0;
+        vector<bool> visit(n);
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                numberOfConnectedComponents++;
+                dfs(i, adj, visit);
             }
         }
-        return conn;
+
+        return numberOfConnectedComponents - 1;
     }
 };
