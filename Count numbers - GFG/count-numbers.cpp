7437d466a0afa83d3ee7649d1c2fb3bbcac25444
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-08-19 02:54:41 +0530

    Added solution - LeetHub

--- Count numbers - GFG/count-numbers.cpp
+++ Count numbers - GFG/count-numbers.cpp
@@ -5,30 +5,32 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
 public:
-bool ThreeCheck(long num)
-{
-    while(num>0){
-            long r = num % 10;
-            if (r ==3) {
-                return true;
-            }
-            num /= 10;
-        }
-        return false;
-}
-    int count(int N){
-        // code here
-        long count = 0;
-        for(int i = 0;i<=N;i++){
-            
-            if(ThreeCheck(i)){
-                count++;
-            }
-        }
-        long ans = N -count;
-        return ans;
+    int count(int N)
+    {
+         // Base cases (Assuming N is not negative)  
+        if (N < 3)  
+            return N;  
+        if (N >= 3 && N < 10)  
+            return N-1;  
+      
+        // Calculate 10^(d-1) (10 raise to the power d-1) where d is  
+        // number of digits in N. po will be 100 for N = 578  
+        int po = 1;  
+        while (N/po > 9)  
+            po = po*10;  
+      
+        // find the most significant digit (msd is 5 for 578)  
+        int msd = N/po;  
+      
+        if (msd != 3)  
+            // For 578, total will be 4*count(10^2 - 1) + 4 + count(78)  
+            return count(msd)*count(po - 1) + count(msd) + count(N%po);  
+        else
+            // For 35, total will be equal to count(29)  
+            return count(msd*po - 1);
     }
 };
+
 
 //{ Driver Code Starts.
 int main() 
