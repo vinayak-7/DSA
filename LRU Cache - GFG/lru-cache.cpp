4ae756929d793700ef5b098d0a6ce1b3c430daac
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-08-19 17:58:24 +0530

    Added solution - LeetHub

--- LRU Cache - GFG/lru-cache.cpp
+++ LRU Cache - GFG/lru-cache.cpp
@@ -4,95 +4,111 @@ using namespace std;
 
 
 // } Driver Code Ends
-// design the class in the most optimal way
-class Node{
-    public:
+struct Node {
     int key;
-    int val;
-    Node *prev;
-    Node *next;
-    Node(int k,int value)
-    {
-        key = k;
-        val = value;
-        prev = NULL;
-        next = NULL;
+    int value;
+    Node *next, *pre;
+    Node(int key, int value) {
+        this->key = key;
+        this->value = value;
+        next = pre = NULL;
     }
 };
+
 class LRUCache
 {
     private:
-    Node* head = new Node(0,0);
-    Node* tail = new Node(0,0); 
-    unordered_map<int,Node*> mp;
-    int capacity;
+    static unordered_map<int, Node *> hsmap;
+    static int capacity, count;
+    static Node *head, *tail;
+
     public:
-    
-    
-    void addNode(int key, int val)
-    {
-        Node *createNode = new Node(key,val);
-        Node *temp = head->next;
-        head->next = createNode;
-        temp->prev = createNode;
-        createNode->next = temp;
-        createNode->prev = head;
-    }
-    
-    
-    void deleteNode(Node* &node)
-    {
-        Node *pre = node->prev;
-        Node *post = node->next;
-        pre->next = post;
-        post->prev = pre;
-        delete(node);
-    }
-    
     //Constructor for initializing the cache capacity with the given value.
     LRUCache(int cap)
     {
+        unordered_map<int, Node *> temp;
+        hsmap = temp;
         capacity = cap;
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
         head->next = tail;
-        tail->prev = head;
+        head->pre = NULL;
+        tail->next = NULL;
+        tail->pre = head;
+        count = 0;
+    }
+
+    void addToHead(Node *node)
+    {
+        node->next = head->next;
+        node->next->pre = node;
+        node->pre = head;
+        head->next = node;
+    }
+
+    //Function to delete a node.
+    void deleteNode(Node *node)
+    {
+        node->pre->next = node->next;
+        node->next->pre = node->pre;
     }
     
     //Function to return value corresponding to the key.
     int GET(int key)
     {
-        if(mp.find(key) != mp.end())
+        //if element is present in map,
+        if (hsmap.count(key) > 0)
         {
-            Node *foundNode = mp[key];
-            int ans = foundNode->val;
-            deleteNode(foundNode);
-            addNode(key,ans);
-            mp.erase(key);
-            mp[key] = head->next;
-            return ans;
+            Node *node = hsmap[key];
+            int result = node->value;
+            
+            deleteNode(node);
+            addToHead(node);
+            
+            //returning the value.
+            return result;
         }
+        //else we return -1.
         return -1;
     }
-    
+
     //Function for storing key-value pair.
     void SET(int key, int value)
     {
-        
-        if(mp.find(key) != mp.end())
+        if (hsmap.count(key) > 0)
         {
-            Node *toDelete = mp[key];
-            mp.erase(key);
-            deleteNode(toDelete);
+            Node *node = hsmap[key];
+            node->value = value;
+            deleteNode(node);
+            addToHead(node);
         }
-        else if(mp.size() == capacity)
+        else
         {
-            Node *toDelete = tail->prev;
-            mp.erase(toDelete->key);
-            deleteNode(toDelete);
+            Node *node = new Node(key, value);
+            hsmap[key] = node;
+            if (count < capacity) {
+                count++;
+                addToHead(node);
+            } 
+            else {
+                hsmap.erase(tail->pre->key);
+                deleteNode(tail->pre);
+                addToHead(node);
+            }
         }
-        addNode(key,value);
-        mp[key] = head->next;
-    } 
+    }
 };
+
+//initializing static members.
+unordered_map<int, Node *> temp;
+int LRUCache::capacity = 0;
+Node *LRUCache::head = new Node(0, 0);
+Node *LRUCache::tail = new Node(0, 0);
+int LRUCache::count = 0;
+unordered_map<int, Node *> LRUCache::hsmap = temp;
+
+
+
 
 //{ Driver Code Starts.
 
