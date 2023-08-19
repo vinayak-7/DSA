650205f200018274f3afc97f1a40dfc87c2a80d9
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-08-19 18:10:15 +0530

    Added solution - LeetHub

--- Reverse First K elements of Queue - GFG/reverse-first-k-elements-of-queue.cpp
+++ Reverse First K elements of Queue - GFG/reverse-first-k-elements-of-queue.cpp
@@ -33,20 +33,46 @@ int main() {
 // Function to reverse first k elements of a queue.
 queue<int> modifyQueue(queue<int> q, int k) {
     // add code here.
-    vector<int>res;
-    int i=0;
-    while(i<k){
-        res.push_back(q.front());
+    // vector<int>res;
+    // int i=0;
+    // while(i<k){
+    //     res.push_back(q.front());
+    //     q.pop();
+    //     i++;
+    // }
+    // queue<int>ans;
+    // for(int i=k-1;i>=0;i--){
+    //     ans.push(res[i]);
+    // }
+    // while(!(q.empty())){
+    //     ans.push(q.front());
+    //     q.pop();
+    // }
+    // return ans;
+    stack<int> s;
+    queue<int> qq;
+
+    // we pop first k elements from queue and push it in the stack.
+    while (k-- > 0) {
+        int a = q.front();
         q.pop();
-        i++;
+        s.push(a);
     }
-    queue<int>ans;
-    for(int i=k-1;i>=0;i--){
-        ans.push(res[i]);
+
+    // while stack is not empty, we push the elements into the new queue.
+    while (!s.empty()) {
+        int a = s.top();
+        s.pop();
+        qq.push(a);
     }
-    while(!(q.empty())){
-        ans.push(q.front());
+
+    // then we add rest of the elements of original queue to the new queue.
+    while (!q.empty()) {
+        int a = q.front();
         q.pop();
+        qq.push(a);
     }
-    return ans;
+
+    // returning the new queue.
+    return qq;
 }
