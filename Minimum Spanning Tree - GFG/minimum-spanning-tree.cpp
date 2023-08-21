08a444ec9114089848f0d61feea4cef29ab1e223
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-08-21 17:30:02 +0530

    Added solution - LeetHub

---
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -0,0 +1,143 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class DisjointSet{
+    vector<int> rank,parent,size;
+    public:
+    DisjointSet(int n){
+        rank.resize(n+1,0);
+        size.resize(n+1);
+        parent.resize(n+1);
+        for(int i = 0; i <= n ; i++)
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+    int findUPar(int node){
+        if(node == parent[node]) return node;
+        
+        return parent[node] = findUPar(parent[node]);
+    }
+    
+    void unionByRank(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        
+        if(ulp_u == ulp_v) return;
+        
+        if(rank[ulp_u] < rank[ulp_v]) parent[ulp_u] = ulp_v;
+        else if(rank[ulp_u] > rank[ulp_v]) parent[ulp_v] = ulp_u;
+        else{
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+};
+class Solution
+{
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree using prims algorithm.
+    // int spanningTree(int v, vector<vector<int>> adj[])
+    // {
+    //     // code here
+    //     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        
+    //     vector<int> vis(v,0);
+    //     // {wt,node}
+    //     pq.push({0,0});
+    //     int sum = 0;
+    //     while(!pq.empty())
+    //     {
+    //         auto it = pq.top();
+    //         pq.pop();
+    //         int wt = it.first;
+    //         int node = it.second;
+            
+    //         if(vis[node] == 1) continue;
+    //         vis[node] = 1;
+    //         sum += wt;
+            
+    //         for(auto it: adj[node])
+    //         {
+    //             int adjNode = it[0];
+    //             int edw = it[1];
+    //             if(!vis[adjNode])
+    //             {
+    //                 pq.push({edw,adjNode});
+    //             }
+    //         }
+    //     }
+    //     return sum;
+    // }
+    // Kruskal's Algorithm
+    int spanningTree(int v, vector<vector<int>> adj[]){
+        vector<pair<int,pair<int,int>>> edges;
+        for(int i = 0 ; i < v ; i++)
+        {
+            for(auto it:adj[i]){
+                int adjNode = it[0];
+                int wt = it[1];
+                int node = i;
+                
+                edges.push_back({wt,{node, adjNode}});
+            }
+        }
+        DisjointSet ds(v);
+        sort(edges.begin(),edges.end());
+        
+        int mstWt = 0;
+        
+        for(auto it: edges){
+            int wt = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
+            
+            if(ds.findUPar(u) != ds.findUPar(v)){
+                mstWt += wt;
+                ds.unionByRank(u,v);
+            }
+        }
+        
+        return mstWt;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        
+        Solution obj;
+    	cout << obj.spanningTree(V, adj) << "\n";
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
