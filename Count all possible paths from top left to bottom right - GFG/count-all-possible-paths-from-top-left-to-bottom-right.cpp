74167606c933a7f19ccdd9367ad3dd61dc37fbb1
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-09-05 16:39:41 +0530

    Added solution - LeetHub

--- Count all possible paths from top left to bottom right - GFG/count-all-possible-paths-from-top-left-to-bottom-right.cpp
+++ Count all possible paths from top left to bottom right - GFG/count-all-possible-paths-from-top-left-to-bottom-right.cpp
@@ -7,22 +7,40 @@ using namespace std;
 // User function Template for C++
 class Solution {
   public:
-    long long f(int m , int n , vector<vector<long long>> &dp)
-    {
-        int mod = 1e9 + 7;
-        if(m == 0 && n == 0) return 1;
-        if(m < 0 || n < 0) return 0;
-        if(dp[m][n] != -1) return dp[m][n];
+    // long long f(int m , int n , vector<vector<long long>> &dp)
+    // {
+    //     int mod = 1e9 + 7;
+    //     if(m == 0 && n == 0) return 1;
+    //     if(m < 0 || n < 0) return 0;
+    //     if(dp[m][n] != -1) return dp[m][n];
         
-        long long up = f(m-1,n,dp);
-        long long left = f(m,n-1,dp);
+    //     long long up = f(m-1,n,dp);
+    //     long long left = f(m,n-1,dp);
         
-        return dp[m][n] = (up + left)%mod;
-    }
+    //     return dp[m][n] = (up + left)%mod;
+    // }
     long long int numberOfPaths(int m, int n){
         // code here
-        vector<vector<long long>> dp( m , vector<long long> (n, -1)); 
-        return f(m-1,n-1,dp);
+        vector<vector<long long>> dp( m , vector<long long> (n, -1));
+        int mod = 1e9 + 7;
+        for(long long i = 0 ; i < m ; i++)
+        {
+            for(long long j = 0 ; j < n ; j++)
+            {
+                
+                if(i == 0 && j == 0) dp[0][0] = 1;
+                else
+                {
+                    long long up = 0;
+                    long long left = 0;
+                    if(i>0)up = dp[i-1][j];
+                    if(j>0)left = dp[i][j-1];
+                    dp[i][j] = (up + left)%mod;
+                }
+                
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
 
