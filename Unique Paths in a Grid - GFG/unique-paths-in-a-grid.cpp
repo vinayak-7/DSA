3882f437202c2efc230511abcc62e3db3ff55ad4
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-09-05 19:10:52 +0530

    Added solution - LeetHub

--- Unique Paths in a Grid - GFG/unique-paths-in-a-grid.cpp
+++ Unique Paths in a Grid - GFG/unique-paths-in-a-grid.cpp
@@ -23,26 +23,48 @@ class Solution {
     // }
     int uniquePaths(int n, int m, vector<vector<int>> &grid) {
         // code here
-        vector<vector<int>> dp( n , vector<int> (m, -1));
+        // vector<vector<int>> dp( n , vector<int> (m, -1));
         
+        // int mod = 1e9 + 7;
+        // for(long long i = 0 ; i < n ; i++)
+        // {
+        //     for(long long j = 0 ; j < m ; j++)
+        //     {
+        //         if(grid[i][j] == 0) dp[i][j] = 0;
+        //         else if(i == 0 && j == 0) dp[i][j] = 1;
+        //         else
+        //         {
+        //             long long up = 0;
+        //             long long left = 0;
+        //             if(i>0)up = dp[i-1][j];
+        //             if(j>0)left = dp[i][j-1];
+        //             dp[i][j] = (up + left)%mod;
+        //         }
+        //     }
+        // }
+        // return dp[n-1][m-1];
         int mod = 1e9 + 7;
-        for(long long i = 0 ; i < n ; i++)
+        vector<int> prev(m,0);
+
+        for(int i = 0 ; i < n ; i++)
         {
-            for(long long j = 0 ; j < m ; j++)
+            vector<int> curr(m,0);
+            for(int j = 0 ; j < m ; j++)
             {
-                if(grid[i][j] == 0) dp[i][j] = 0;
-                else if(i == 0 && j == 0) dp[i][j] = 1;
+                if(grid[i][j] == 0) curr[j] = 0;
+                else if(i == 0 && j == 0) curr[j] = 1;
                 else
                 {
-                    long long up = 0;
-                    long long left = 0;
-                    if(i>0)up = dp[i-1][j];
-                    if(j>0)left = dp[i][j-1];
-                    dp[i][j] = (up + left)%mod;
+                    int up = 0;
+                    int left = 0;
+                    if(i>0)up = prev[j];
+                    if(j>0)left = curr[j-1];
+                    curr[j] = (up + left)%mod;
                 }
             }
+            prev = curr;
         }
-        return dp[n-1][m-1];
+        return prev[m-1];
     }
 };
 
