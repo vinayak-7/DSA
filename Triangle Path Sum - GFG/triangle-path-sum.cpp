d07a10281d7c2189de68c65e5e4fbd1beea36b72
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-09-06 19:31:06 +0530

    Added solution - LeetHub

--- Triangle Path Sum - GFG/triangle-path-sum.cpp
+++ Triangle Path Sum - GFG/triangle-path-sum.cpp
@@ -5,22 +5,37 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int f(int i , int j , vector<vector<int>>& mat,vector<vector<int>>& dp)
-    {
-        int n = mat.size();
-        if(i == n-1) return mat[n-1][j];
-        if(dp[i][j] != -1) return dp[i][j];
+    // int f(int i , int j , vector<vector<int>>& mat,vector<vector<int>>& dp)
+    // {
+    //     int n = mat.size();
+    //     if(i == n-1) return mat[i][j];
+    //     if(dp[i][j] != -1) return dp[i][j];
         
-        int down = mat[i][j] + f(i+1,j,mat,dp);
-        int diag = mat[i][j] + f(i+1,j+1,mat,dp);
+    //     int down = mat[i][j] + f(i+1,j,mat,dp);
+    //     int diag = mat[i][j] + f(i+1,j+1,mat,dp);
         
-        return dp[i][j] = min(down,diag);
+    //     return dp[i][j] = min(down,diag);
         
-    }
+    // }
     int minimizeSum(int n, vector<vector<int>>& mat) {
         // Code here
-        vector<vector<int>> dp(n , vector<int>(n,-1));
-        return f(0,0,mat,dp);
+        // vector<vector<int>> dp(n , vector<int>(n,0));
+        vector<int> front(n,0),curr(n,0);
+        for(int i = 0 ; i < n ; i++)
+        {
+            front[i] = mat[n-1][i];
+        }
+        for(int i = n-2 ; i >= 0 ; i--){
+            for(int j = i ; j >= 0 ; j--)
+            {
+                int down = mat[i][j] + front[j];
+                int diag = mat[i][j] + front[j+1];
+                
+                curr[j] = min(down,diag);
+            }
+            front = curr;
+        }
+        return front[0];
     }
 };
 
