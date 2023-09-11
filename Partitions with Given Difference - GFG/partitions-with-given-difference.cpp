c28e16f604e5201e4a7bfbcda4d3cccf20ea88df
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-09-12 00:50:50 +0530

    Added solution - LeetHub

--- Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
+++ Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
@@ -5,29 +5,21 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-//     int f(int ind , int target , vector<int>& arr , vector<vector<int>> &dp)
-// 	{
-// 	    int mod = 1e9+7;
-// 	    if(ind == 0)
-// 	    {
-// 	        if(arr[0] == 0 and target == 0) return 2;
-// 	        if(target == 0 or target == arr[0]) return 1;
-// 	        return 0;
-// 	    }
-// 	    if(dp[ind][target] != -1) return dp[ind][target];
-	    
-// 	    int not_taken = f(ind - 1 , target, arr, dp);
-// 	    int taken = 0;
-// 	    if(arr[ind] <= target) taken = f(ind - 1, target - arr[ind] , arr , dp);
-	    
-// 	    return dp[ind][target] = (taken + not_taken)%mod;
-// 	}
+
+    int countPartitions(int n, int d, vector<int>& arr) {
+        // Code here
+        int totSum = 0;
+        for (int i = 0; i < n; i++) 
+        {
+            totSum += arr[i];
+        }
+        if(totSum - d < 0 || (totSum - d)%2) return false;
     
-	int perfectSum(int n, int tar, vector<int>& arr)
-	{
-        // Your code goes here
+        int tar = (totSum-d)/2;
         int mod = 1e9+7;
+        
         vector<vector<int>> dp(n,vector<int>(tar + 1 , 0));
+        
         if (arr[0] == 0) dp[0][0] = 2;
         else dp[0][0] = 1;
         if(arr[0] != 0 && arr[0] <= tar) dp[0][arr[0]] = 1;
@@ -44,18 +36,6 @@ class Solution {
             
         }
         return dp[n-1][tar];
-	}
-    
-    int countPartitions(int n, int d, vector<int>& arr) {
-        // Code here
-        int totSum = 0;
-        for (int i = 0; i < n; i++) 
-        {
-            totSum += arr[i];
-        }
-        if(totSum - d < 0 || (totSum - d)%2) return false;
-    
-        return perfectSum(n,(totSum-d)/2,arr);
     }
 };
 
