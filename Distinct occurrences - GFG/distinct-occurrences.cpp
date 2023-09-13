a2de3cac3da7ecef5bb078f8a65815896819488e
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-09-14 01:34:17 +0530

    Added solution - LeetHub

--- Distinct occurrences - GFG/distinct-occurrences.cpp
+++ Distinct occurrences - GFG/distinct-occurrences.cpp
@@ -10,24 +10,37 @@ class Solution
 {
     int mod = 1e9+7;
     public:
-    int count(int i , int j , string& s1 , string& s2,vector<vector<int>> &dp)
-    {
-        if(j < 0) return 1;
-        if(i < 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s1[i] == s2[j])
-        {
-            return dp[i][j] = (count(i-1,j-1,s1,s2,dp)%mod + count(i-1,j,s1,s2,dp)%mod)%mod;
-        }
-        else return dp[i][j] = count(i-1,j,s1,s2,dp)%mod;
-    }
+    // int count(int i , int j , string& s1 , string& s2,vector<vector<int>> &dp)
+    // {
+    //     if(j < 0) return 1;
+    //     if(i < 0) return 0;
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     if(s1[i] == s2[j])
+    //     {
+    //         return dp[i][j] = (count(i-1,j-1,s1,s2,dp)%mod + count(i-1,j,s1,s2,dp)%mod)%mod;
+    //     }
+    //     else return dp[i][j] = count(i-1,j,s1,s2,dp)%mod;
+    // }
     int subsequenceCount(string s1, string s2)
     {
       //Your code here
-      int n = s1.size();
-      int m = s2.size();
-      vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-      return count(n,m,s1,s2,dp);
+        int n = s1.size();
+        int m = s2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i = 0 ; i <= n ; i++) dp[i][0] = 1;
+        for(int j = 1 ; j <= m ; j++) dp[0][j] = 0;
+        for(int i = 1 ; i <= n ; i++)
+        {
+            for(int j = 1 ; j <= m ; j++)
+            {
+                if(s1[i-1] == s2[j-1])
+                {
+                    dp[i][j] = (dp[i-1][j-1]%mod + dp[i-1][j]%mod)%mod;
+                }
+                else dp[i][j] = dp[i-1][j]%mod;
+            }
+        }
+        return dp[n][m];
     }
 };
  
