43f241fd4eaacb8548e858f39652e191c74d5e71
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-09-15 17:12:20 +0530

    Time: 272 ms (63.30%), Space: 132.9 MB (61.96%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,65 +1,31 @@
 class Solution {
 public:
-//     int maxP(int ind , int buy ,int cap ,vector<int>& prices,vector<vector<vector<int>>>& dp)
-//     {
-//         int n = prices.size();
-//         if(ind == n) return 0;
-//         if(cap == 0) return 0;
+    int maxP(int ind , int trans ,vector<int>& prices,vector<vector<int>>& dp)
+    {
+        int n = prices.size();
+        if(ind == n || trans == 4) return 0;
         
-//         int profit = 0;
+        int profit = 0;
         
-//         if(dp[ind][buy][cap] != -1) return dp[ind][buy][cap];
+        if(dp[ind][trans] != -1) return dp[ind][trans];
         
-//         if(buy)
-//         {
-//             profit = max(-prices[ind] + maxP(ind+1,0,cap,prices,dp),
-//                          maxP(ind+1,1,cap,prices,dp));
-//         }
-//         else
-//         {
-//             profit = max(prices[ind] + maxP(ind+1,1,cap-1,prices,dp),
-//                          maxP(ind+1,0,cap,prices,dp));
-//         }
+        if(trans % 2 == 0)
+        {
+            profit = max(-prices[ind] + maxP(ind+1,trans+1,prices,dp),
+                         maxP(ind+1,trans,prices,dp));
+        }
+        else
+        {
+            profit = max(prices[ind] + maxP(ind+1,trans+1,prices,dp),
+                         maxP(ind+1,trans,prices,dp));
+        }
         
-//         return dp[ind][buy][cap] = profit;
+        return dp[ind][trans] = profit;
         
-//     }
+    }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
-        for(int buy = 0 ; buy <= 1 ; buy++)
-        {
-            for(int cap = 0 ; cap <= 2 ; cap++)
-            {
-                dp[n][buy][cap] = 0;
-            }
-        }
-        for(int ind = n-1 ; ind >= 0 ; ind--)
-        {
-            for(int buy = 0 ; buy <= 1 ; buy++)
-            {
-                dp[ind][buy][0] = 0;
-            }
-        }
-        for(int ind = n-1 ; ind >= 0 ; ind--)
-        {
-            for(int buy = 0 ; buy <= 1 ; buy++)
-            {
-                for(int cap = 1 ; cap <= 2 ;cap++)
-                {
-                    if(buy)
-                    {
-                        dp[ind][buy][cap] = max(-prices[ind] + dp[ind+1][0][cap],
-                                     dp[ind+1][1][cap]);
-                    }
-                    else
-                    {
-                        dp[ind][buy][cap] = max(prices[ind] + dp[ind+1][1][cap-1],
-                                     dp[ind+1][0][cap]);
-                    }
-                }
-            }
-        }
-        return dp[0][1][2];
+        vector<vector<int>> dp(n,vector<int>(4,-1));
+        return maxP(0,0,prices,dp);
     }
 };
