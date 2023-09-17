74db02989edabe391f19f6b8236e4a324e01e250
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-09-18 01:25:12 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -9,23 +9,38 @@ using namespace std;
 
 class Solution{
 public:
-    int matrixMulti(int i , int j , int arr[] , int n,vector<vector<int>> &dp)
-    {
-        if(i == j) return 0;
-        int mini = 1e9;
-        if(dp[i][j] != -1) return dp[i][j];
-        for(int k = i ; k < j ; k++)
-        {
-            int steps = (arr[i-1]*arr[k]*arr[j]) + matrixMulti(i,k,arr,n,dp) + matrixMulti(k+1,j,arr,n,dp);
-            mini = min(mini,steps);
-        }
-        return dp[i][j] = mini;
-    }
+    // int matrixMulti(int i , int j , int arr[] , int n,vector<vector<int>> &dp)
+    // {
+    //     if(i == j) return 0;
+    //     int mini = 1e9;
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     for(int k = i ; k < j ; k++)
+    //     {
+    //         int steps = (arr[i-1]*arr[k]*arr[j]) + matrixMulti(i,k,arr,n,dp) + matrixMulti(k+1,j,arr,n,dp);
+    //         mini = min(mini,steps);
+    //     }
+    //     return dp[i][j] = mini;
+    // }
     int matrixMultiplication(int n, int arr[])
     {
         // code here
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return matrixMulti(1,n-1,arr,n,dp);
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        // int dp[n][n];
+        // for(int i = 0 ; i < n-1 ; i++) dp[i][i] = 0;
+        for(int i = n-1 ; i >= 1 ; i--)
+        {
+            for(int j = i+1 ; j < n ; j++)
+            {
+                int mini = 1e9;
+                for(int k = i ; k < j ; k++)
+                {
+                    int steps = (arr[i-1]*arr[k]*arr[j]) + dp[i][k] + dp[k+1][j];
+                    mini = min(mini,steps);
+                }
+                dp[i][j] = mini;
+            }
+        }
+        return dp[1][n-1];
     }
 };
 
