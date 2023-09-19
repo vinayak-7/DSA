7db93cc6595342660b36c58187bc3fc868cc6a0c
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-09-19 22:55:13 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Partition Array for Maximum Sum/partition-array-for-maximum-sum.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int f(int i , int k , int n , vector<int> &arr, vector<int> &dp)
+    {
+        if(i == n) return 0;
+        if(dp[i] != -1) return dp[i];
+        int len = 0 ,maxi = INT_MIN ,maxAns = INT_MIN;
+        for(int j = i ; j < min(n,i+k) ; j++)
+        {
+            len++;
+            maxi = max(maxi,arr[j]);
+            int sum = len * maxi + f(j+1,k,n,arr,dp);
+            maxAns = max(maxAns,sum);
+        }
+        
+        return dp[i] = maxAns;
+    }
+    int solve(int n, int k, vector<int>& arr){
+        // Code here
+        vector<int> dp(n,-1);
+        return f(0,k,n,arr,dp);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k; cin>>n>>k;
+        vector<int> arr;
+        
+        for(int i=0; i<n; ++i){
+            int x; cin>>x;
+            arr.push_back(x);
+        }
+        
+        Solution obj;
+        cout<<obj.solve(n,k,arr)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
