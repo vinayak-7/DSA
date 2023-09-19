928ec6a5aa32bd4a89675ebedffb4d455b74c804
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-09-19 23:00:40 +0530

    Added solution - LeetHub

--- Partition Array for Maximum Sum - GFG/partition-array-for-maximum-sum.cpp
+++ Partition Array for Maximum Sum - GFG/partition-array-for-maximum-sum.cpp
@@ -5,25 +5,39 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    int f(int i , int k , int n , vector<int> &arr, vector<int> &dp)
-    {
-        if(i == n) return 0;
-        if(dp[i] != -1) return dp[i];
-        int len = 0 ,maxi = INT_MIN ,maxAns = INT_MIN;
-        for(int j = i ; j < min(n,i+k) ; j++)
-        {
-            len++;
-            maxi = max(maxi,arr[j]);
-            int sum = len * maxi + f(j+1,k,n,arr,dp);
-            maxAns = max(maxAns,sum);
-        }
+    // int f(int i , int k , int n , vector<int> &arr, vector<int> &dp)
+    // {
+    //     if(i == n) return 0;
+    //     if(dp[i] != -1) return dp[i];
+    //     int len = 0 ,maxi = INT_MIN ,maxAns = INT_MIN;
+    //     for(int j = i ; j < min(n,i+k) ; j++)
+    //     {
+    //         len++;
+    //         maxi = max(maxi,arr[j]);
+    //         int sum = len * maxi + f(j+1,k,n,arr,dp);
+    //         maxAns = max(maxAns,sum);
+    //     }
         
-        return dp[i] = maxAns;
-    }
+    //     return dp[i] = maxAns;
+    // }
     int solve(int n, int k, vector<int>& arr){
         // Code here
-        vector<int> dp(n,-1);
-        return f(0,k,n,arr,dp);
+        vector<int> dp(n+1,0);
+        for(int i = n-1 ; i >= 0 ; i--)
+        {
+            int len = 0 ,maxi = INT_MIN ,maxAns = INT_MIN;
+            for(int j = i ; j < min(n,i+k) ; j++)
+            {
+                len++;
+                maxi = max(maxi,arr[j]);
+                int sum = len * maxi + dp[j+1];
+                maxAns = max(maxAns,sum);
+            }
+            
+            dp[i] = maxAns;
+        }
+        
+        return dp[0];
     }
 };
 
