a2a19a9e625ccadc6cc089155ca9a1ca23ab645b
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-09-30 15:53:13 +0530

    Added solution - LeetHub

--- Boolean Matrix - GFG/boolean-matrix.cpp
+++ Boolean Matrix - GFG/boolean-matrix.cpp
@@ -12,45 +12,37 @@ class Solution
     //is 1 then all the cells in its ith row and jth column will become 1.
     void booleanMatrix(vector<vector<int> > &matrix)
     {
-        // code here 
-        int m = matrix.size(), n = matrix[0].size();
-        bool col = true;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
+        int R = matrix.size();
+        int C = matrix[0].size();
+        
+        vector<int> row(R,0);
+        vector<int> col(C,0);
+
+        
+        for(int i = 0; i < R; i++)
+        {
+            for(int j = 0; j < C; j++)
+            {
+                //if we get 1 in matrix, we mark ith row and jth column as 1.
                 if(matrix[i][j] == 1){
-                    matrix[i][0] = 1;
-                    if(j != 0){
-                        matrix[0][j] = 1;
-                    }
-                    else{
-                        col = false;
-                    }
+                    row[i] = col[j] = 1;
                 }
             }
         }
-        
-        for(int i = 1; i < m; i++){
-            for(int j = 1; j < n; j++){
-                if(matrix[i][0] == 1 || matrix[0][j] == 1){
+
+        for(int i = 0; i < R; i++)
+        {
+            for(int j = 0; j < C; j++)
+            {
+                //if ith row or jth column is marked as 1, then all elements
+                //of matrix in that row and column will be 1.
+                if(row[i] || col[j]){
                     matrix[i][j] = 1;
                 }
             }
         }
-        
-        if(matrix[0][0] == 1){
-            for(int j = 0; j < n; j++){
-                matrix[0][j] = 1;
-            }
-        }
-        
-        if(col == false){
-            for(int i = 0; i < m; i++){
-                matrix[i][0] = 1;
-            }
-        }
     }
 };
-
 
 //{ Driver Code Starts.
 int main() {
