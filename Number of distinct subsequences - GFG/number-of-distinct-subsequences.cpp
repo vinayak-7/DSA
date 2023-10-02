453d3d223bc5a445bf49e4a3c13e2e919b8d2a83
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-10-02 18:03:24 +0530

    Added solution - LeetHub

--- Number of distinct subsequences - GFG/number-of-distinct-subsequences.cpp
+++ Number of distinct subsequences - GFG/number-of-distinct-subsequences.cpp
@@ -4,30 +4,38 @@ using namespace std;
 
 
 // } Driver Code Ends
-//User function template for C++
+//Back-end complete function template for C++
 
 class Solution{
-  public:	
-	int distinctSubsequences(string s)
+  public:
+	int distinctSubsequences(string str)
 	{
-	    // Your code goes here
-	    int mod = 1000000007;
-	    int n = s.length();
-	    vector<int>dp(n+1);
-	    dp[0] =1;
-	    
-	    unordered_map<char,int>mp;
-	    for(int i=1; i<=n;i++){
-	        char ch = s[i-1];
-	        dp[i] = (2*dp[i-1])%mod;
-	        if(mp.find(ch)!=mp.end()){
-	            int index = mp[ch];
-	            dp[i] = (dp[i]-dp[index-1]+mod)%mod;
-	        }
-	        mp[ch] =i;
-	    }
-	    return dp[n];
+		vector<int> last(26, -1); 
+
+		int mod = 1e9 + 7;
+	    int n = str.size(); 
+	    long long dp[n + 1]; 
+	  
+	    // Empty substring has only one subsequence 
+	    dp[0] = 1; 
+	  
+	    // Traverse through all lengths from 1 to n. 
+	    for (int i = 1; i <= n; i++) { 
+	        
+	        dp[i] = 2 * dp[i - 1]; 
+	        dp[i] %= mod;
+	        if (last[str[i - 1] - 'a'] != -1) 
+	            dp[i] = (dp[i] - dp[last[str[i - 1] - 'a']] + mod)%mod; 
+	  
+	        // Mark occurrence of current character 
+	        last[str[i - 1] - 'a'] = (i - 1); 
+	    } 
+	  
+	    return (int)(dp[n]);
 	}
+
+
+   
 };
 
 //{ Driver Code Starts.
