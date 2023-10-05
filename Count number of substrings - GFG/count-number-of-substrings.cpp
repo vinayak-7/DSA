c84b4c71a4e29cc8c483c109d4f4976ca7e10402
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-10-05 23:36:26 +0530

    Added solution - LeetHub

--- Count number of substrings - GFG/count-number-of-substrings.cpp
+++ Count number of substrings - GFG/count-number-of-substrings.cpp
@@ -6,40 +6,40 @@ using namespace std;
 
 
 // } Driver Code Ends
-//User function template for C++
+//Backend complete function template for C++
 
 class Solution
 {
   public:
-    long long substrAtMostK(string s,int k){
-        int n = s.size();
-        int i = 0;
-        int j = 0;
-        vector<int> hash(26,0);
-        int cnt = 0;
-        int res = 0;
-        while(j<n){
-           hash[s[j]-'a']++;
-           if(hash[s[j]-'a']==1){//first time entered in hash so one distinct character enters
-               cnt++;
-           }
-           if(cnt>k){
-               while(cnt>k){
-                   hash[s[i]-'a']--;
-                   if(hash[s[i]-'a']==0){
-                       cnt--;
-                   }
-                   i++;
-               }
-           }
-           res+=(j-i+1);
-           j++;
-        }
-        return res;
+    long long int atMostK (string s, int k)
+    {
+    	if (k < 0) return 0;
+    
+    	int i = 0, j = 0, cnt = 0;
+    	long long int res = 0;
+    	int m[26] = {0};
+    
+    	while (j < s.length ())
+    	{
+    		m[s[j] - 'a']++;
+    		if (m[s[j] - 'a'] == 1) cnt++;
+    
+    		while (cnt > k)
+    		{
+    			m[s[i] - 'a']--;
+    			if (m[s[i] - 'a'] == 0) cnt--;
+    
+    			i++;
+    		}
+    
+    		res += (j - i + 1);
+    		j++;
+    	}
+    	return res;
     }
+
     long long int substrCount (string s, int k) {
-    	//code here.
-    	return substrAtMostK(s,k) - substrAtMostK(s,k-1);
+    	return atMostK (s, k) - atMostK (s, k - 1);
     }
 };
 
