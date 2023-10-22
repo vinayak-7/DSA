446d13d9dd450cc8549dd1ca9c9885db71e6f977
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-10-22 23:04:38 +0530

    Added solution - LeetHub

--- Number of paths - GFG/number-of-paths.cpp
+++ Number of paths - GFG/number-of-paths.cpp
@@ -4,23 +4,41 @@ using namespace std;
  
 
 // } Driver Code Ends
+class Solution
+{
+    public:
+    long long power(long long x,int y, int p)
+    {
+        long long res = 1; // Initialize result
+        while (y > 0)
+        {
+            // If y is odd, multiply x with result
+            if (y & 1)
+                res = (res * x) % p;
+            // y must be even now
+            y = y >> 1; // y = y/2
+            x = (x * x) % p;
+        }
+        return res;
+    }
+ 
+    // Returns n^(-1) mod p
+    long long modInverse(long long n,int p)
+    {
+        return power(n, p - 2, p);
+    }
+    long long  numberOfPaths(int M, int N)
+    {
+        long long path = 1,mod=1e9+7;
+        for (long long i = N; i < (M + N - 1); i++) {
+            path = (path*i)%mod;
+            long long inv=modInverse(i-N+1,mod);
+            path = (path*inv)%mod;
+        }
+        return path;
+    }
+};
 
-long long f(int m , int n)
-{
-    if(m == 0 && n == 0) return 1;
-    if(m < 0 || n < 0) return 0;
-    
-    long long up = f(m-1,n);
-    long long left = f(m,n-1);
-    
-    return up + left;
-}
-long long  numberOfPaths(int m, int n)
-{
-    // Code Here
-    return f(m-1,n-1);
-    
-}
 
 //{ Driver Code Starts.
 
@@ -31,9 +49,10 @@ int main()
 	cin>>t;
 	while(t--)
 	{
-		int n,m;
-		cin>>m>>n;
-	    cout << numberOfPaths(m, n)<<endl;
+		int N, M;
+		cin>>M>>N;
+		Solution ob;
+	    cout << ob.numberOfPaths(M, N)<<endl;
 	}
     return 0;
 }
