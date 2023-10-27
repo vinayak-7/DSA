0f7094c50f9f9903ea05c434f9ddbe2a3ab8c689
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-10-27 12:08:23 +0530

    Added solution - LeetHub

--- Minimum Deletions - GFG/minimum-deletions.cpp
+++ Minimum Deletions - GFG/minimum-deletions.cpp
@@ -8,30 +8,24 @@ using namespace std;
 
 class Solution{
   public:
-    int lon_palin(string &S, int i, int j, int &n, vector<vector<int>>& dp){
-        if(i > j){
-            return 0;
-        }
-        else if(i == j){
-            return 1;
-        }
-        else if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        else{
-            if(S[i] == S[j]){
-                return dp[i][j] = 2 + lon_palin(S, i+1, j-1, n, dp);
-            }
-            else{
-                return dp[i][j] = max(lon_palin(S, i+1, j, n, dp), lon_palin(S, i, j-1, n, dp));
-            }
-        }
+    int fun(int i , int j , string& s1 , string& s2 , vector<vector<int>> &dp)
+    {
+        if(i < 0 || j < 0) return 0;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        if(s1[i] == s2[j]) return dp[i][j] = 1 + fun(i-1,j-1,s1,s2,dp);
+        
+        else return dp[i][j] = max(fun(i-1,j,s1,s2,dp), fun(i,j-1,s1,s2,dp));
     }
-    int minimumNumberOfDeletions(string S) { 
+    int minimumNumberOfDeletions(string str) { 
         // code here
-        int n = S.size();
+        string temp = str;
+        reverse(temp.begin(),temp.end());
+        int n = temp.size();
         vector<vector<int>>dp(n,vector<int>(n,-1));
-        return n - lon_palin(S, 0, n-1, n, dp);
+
+        return n - fun(n-1,n-1,str,temp,dp);
     }
 };
 
