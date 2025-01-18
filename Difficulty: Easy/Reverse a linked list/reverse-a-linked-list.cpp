4a0a104cac97a317a0c40e2e2cb915a3eb3ae98a
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2025-01-18 21:10:34 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse a linked list/reverse-a-linked-list.cpp
@@ -0,0 +1,120 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <cmath>
+#include <cstdio>
+#include <ios>
+#include <iostream>
+#include <random>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+/* Linked List Node structure:
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}
+
+*/
+
+class Solution
+{
+    public:
+    //Function to reverse a linked list.
+    struct Node* reverseList(struct Node *head)
+    {
+        // code here
+        // return head of reversed list
+        if (head == NULL)
+            return head;
+        Node* curr_node = head;
+        Node* prev_node = NULL;
+        Node* next_node = NULL;
+        
+        while(curr_node != NULL)
+        {
+            next_node = curr_node->next;
+            curr_node->next = prev_node;
+            prev_node = curr_node;
+            curr_node = next_node;
+        }
+        
+        head = prev_node;
+        
+        return head;
+    }
+    
+};
+    
+
+
+
+//{ Driver Code Starts.
+
+void printList(Node *head) {
+    Node *temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+/* Driver program to test above function*/
+int main() {
+    int T, n, l, firstdata;
+    cin >> T;
+    cin.ignore();
+    while (T--) {
+        Node *head = NULL, *tail = NULL;
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int index = 0;
+        n = arr.size();
+        if (n != 0) {
+            head = new Node(arr[0]);
+            tail = head;
+        }
+
+        for (int i = 1; i < n; i++) {
+            l = arr[i];
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+
+        Solution ob;
+        head = ob.reverseList(head);
+
+        printList(head);
+        cout << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
